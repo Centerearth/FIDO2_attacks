7785aa5c60fcd264cc7c98f75d877b1a67b5c26b
Centerearth <andrewtheamazingbrother@gmail.com>
2026-03-05 09:16:26 -0700

    Changed the description in the about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,19 +5,19 @@ export default function AboutPage() {
     <Layout>
       <div id="about-main">
         <p>
-          Gone is the stress you feel every time that you pick classes out for your next semester at BYU. RateMyClass is a RateMyProfessor spinoff that makes the process of choosing easy by giving you all the information you need to make informed decisions. Students who have taken a certain class can post reviews about the difficulty level of the class, professor and TA recommendations, helpful resources, things to watch out for, and really anything else. Students looking to take the class can find the class among a list of all current BYU classes and review the posts made by previous takers to make their BYU stay as smooth as possible.
+          This project is dedicated to exploring the security of the FIDO2 authentication standard. 
+          We have developed a simple shopping website and server that implement FIDO2 to serve as a controlled environment for our research. 
+          Our primary goal is to investigate potential vulnerabilities and attack vectors in FIDO2 implementations. 
+          This work is being conducted as part of a research paper at BYU aimed at creating a taxonomy of all FIDO2 attacks and determining their relevancy. 
         </p>
         <p>
-          Rate My Class is currently authored and updated by Andrew Nicholls
+          2026
         </p>
         <p>
-          2023
+          See <a href="https://github.com/Centerearth/FIDO2_attacks">https://github.com/Centerearth/FIDO2_attacks</a> for the Github repository
         </p>
-        <p>
-          See <a href="https://github.com/Centerearth/startup">https://github.com/Centerearth/startup</a> for the Github repository
-        </p>
-        <p> See
-          <a href="ratemyclass260.link">ratemyclass260.link</a> for Andrew's home page.
+        <p> 
+          See <a href="https://isrl.byu.edu/">https://isrl.byu.edu/</a> for the home page of the Internet Security Research lab (ISRL) at BYU.
         </p>
       </div>
     </Layout>
