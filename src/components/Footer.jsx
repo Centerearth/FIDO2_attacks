70276632a84aacca17fe161568c6b47f0034e383
Centerearth <andrewtheamazingbrother@gmail.com>
2026-03-05 10:05:22 -0700

    Changed spacing in the foter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="bg-dark text-white-50">
-      <div className="container-fluid">
+      <div className="container-fluid d-flex justify-content-between align-items-center h-100">
         <span className="text-reset">Author: ISRL at BYU</span>
         <a className="text-reset" href="https://github.com/Centerearth/FIDO2_attacks">
           Source
