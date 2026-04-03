ff66b6bfe0f2096ab8c97fb1c21e81148eb19323
Andrew Nicholls <andrewtheamazingbrother@gmail.com>
2026-04-03 14:35:49 -0600

    Trying to see if this fixes the images not loading properly

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -38,7 +38,7 @@ export default function HomePage() {
             {products.map((product) => (
               <div key={product.id} className="col-md-3 mb-4">
                 <div className="card h-100 shadow-sm product-card">
-                  <img src={`https://placehold.co/400x300/EFEFEF/333333?text=${product.name.replace(' ', '\\n')}`} className="card-img-top" alt={product.name} />
+                  <img src={`https://placehold.co/400x300/EFEFEF/333333?text=${product.name.replaceAll(' ', '%0A')}`} className="card-img-top" alt={product.name} />
                   <div className="card-body text-start">
                     <h5 className="card-title">{product.name}</h5>
                     <p className="card-text">
